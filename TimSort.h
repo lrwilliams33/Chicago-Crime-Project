7c2f99a1fab1f43bf0754c375b16156edcc48c26
lrwilliams33 <116681252+lrwilliams33@users.noreply.github.com>
2023-11-29 11:04:33 -0500

    tim sort bug fixes and new bug creations! :)

--- TimSort.h
+++ TimSort.h
@@ -9,19 +9,27 @@ using namespace std;
 
 int run = 1; //figure out run variable number afterwards, 1 is just a random number
 
-void insertionSort(parameters){
-
+void insertionSort(vector<Crime*>& crimesVect, int left, int right){
+    for (int i = left + 1; i <= right; i++) {
+        Crime* temp = crimesVect.at(i);
+        int j = i - 1;
+        while (j >= left && crimesVect.at(j)->Block.compare(temp->Block) > 0) {
+            crimesVect.at(j + 1) = crimesVect.at(j);
+            j--;
+        }
+        crimesVect.at(j + 1) = temp;
+    }
 }
 
 //mergesort and timsort function are basically done, just have to do insertion sort
 
-void merge(vector<Crime> &crimesVect, int left, int mid, int right){
+void merge(vector<Crime*> &crimesVect, int left, int mid, int right){
     int n1 = mid - left + 1;
     int n2 = right - mid;
-    vector<Crime> crimes1;
-    vector<Crime> crimes2;
+    vector<Crime*> crimes1(crimesVect.size());
+    vector<Crime*> crimes2(crimesVect.size());
     for(int i = 0; i < n1; i++){
-        crimes1.at(i) = crimesVect.at(left + 1);
+        crimes1.at(i) = crimesVect.at(left + i);
     }
     for(int j = 0; j < n2; j++){
         crimes2.at(j) = crimesVect.at(mid + 1 + j);
@@ -31,7 +39,7 @@ void merge(vector<Crime> &crimesVect, int left, int mid, int right){
     j = 0;
     k = left;
     while( i < n1 && j < n2){
-        if (crimesVect.at(i).Block <= crimesVect.at(j).Block){  // figure out what we are comparing for Crime Objects, not sure if it is just block
+        if (crimesVect.at(i)->Block.compare(crimesVect.at(j)->Block) <= 0){  // figure out what we are comparing for Crime Objects, not sure if it is just block
             crimesVect.at(k) = crimes1.at(i);
             i++;
         }
@@ -53,7 +61,7 @@ void merge(vector<Crime> &crimesVect, int left, int mid, int right){
     }
 }
 
-void mergeSort(vector<Crime> &crimesVect, int left, int right){
+void mergeSort(vector<Crime*> &crimesVect, int left, int right){
     if (left < right){
         int mid = left + (right - left) / 2;
         mergeSort(crimesVect, left, mid);
@@ -62,16 +70,16 @@ void mergeSort(vector<Crime> &crimesVect, int left, int right){
     }
 }
 
-void timSort(vector<Crime> &crimesVect, int vect_size){
+void timSort(vector<Crime*> &crimesVect, int vect_size){
     for (int i = 0; i < crimesVect.size(); i+=run){
-        //insertionSort(arr, i, min((i + run - 1), (vect_size - 1)));
+        insertionSort(crimesVect, i, min((i + run - 1), (vect_size - 1)));
     }
     for (int size = run; size < vect_size; size = 2 * size){
         for (int left = 0; left < vect_size; left += 2 * size){
             int right = min((left + 2 * size - 1), (vect_size - 1));
             int mid = left + size - 1;
             if (mid < right){
-                //merge(arr, left, mid, right);
+                merge(crimesVect, left, mid, right);
             }
         }
     }
