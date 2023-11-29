e530040b6755944fbd2c2e20d2877ff543b4e677
jackw00 <74882779+jackw00@users.noreply.github.com>
2023-11-29 10:43:44 -0500

    final quicksort and testing in main

--- QuickSort.h
+++ QuickSort.h
@@ -2,41 +2,44 @@
 #define CHICAGO_CRIME_PROJECT_QUICKSORT_H
 #include <vector>
 #include <string>
+#include "Crime.h"
 using namespace std;
 
-int partition(vector<string>& crimesVect, int low, int high) {
-    string pivot = crimesVect.at(low);
+void swap(vector<Crime*>& crimesVect, int first, int second){
+    Crime* f = crimesVect.at(first);
+    Crime* s = crimesVect.at(second);
+    crimesVect.at(first) = s;
+    crimesVect.at(second) = f;
+}
+
+int partition(vector<Crime*>& crimesVect, int low, int high) {
+    string pivot = crimesVect.at(low)->Block;
     int up = low;
     int down = high;
 
     while (up < down) {
         for (int j=up; j<high; j++) {
-            if (array[up] > pivot) break;
+            if(crimesVect.at(up)->Block.compare(pivot) > 0)
+                break;
             up++;
         }
-        for (int j=high; j<low; j++) {
-            if (array[down] < pivot) break;
+        for (int j=high; j>low; j--) {
+            if(crimesVect.at(down)->Block.compare(pivot) < 0)
+                break;
             down--;
         }
-        if (up < down) swap(array[up], array[down])
+        if (up < down) swap(crimesVect, up, down);
     }
-    swap(array[low], array[down]);
+    swap(crimesVect, low, down);
     return down;
 }
 
-void swap(vector<string>& crimesVect, string& first, string& second){}
-
-void firstAlphabeticallyGreaterThanSecond(vector<string>& crimesVect, string& first, string& second){} // don't worry this will be a better name
-
-
-void quickSort(vector<string>& crimesVect, int low, int high) {
+void quickSort(vector<Crime*>& crimesVect, int low, int high) {
     if (low < high) {
         int pivot = partition(crimesVect, low, high);
         quickSort(crimesVect, low, pivot - 1);
         quickSort(crimesVect, pivot+1, high);
     }
 }
-
-
 
 #endif //CHICAGO_CRIME_PROJECT_QUICKSORT_H
